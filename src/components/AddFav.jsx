dc3e72e0a2137e24bc63932ebf92b0e740d5271f
sameerasw <sameera.whoami@gmail.com>
2024-08-03 23:02:06 +0530

    Fixed uneditable link input

--- src/components/AddFav.jsx
+++ src/components/AddFav.jsx
@@ -1,34 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function deleteLocalStorage() {
   localStorage.removeItem("links");
-  //   window.location.reload();
   let favUrl = localStorage.getItem("favUrl");
   fetch(favUrl)
     .then((response) => response.json())
     .then((dataNew) => {
-      //if the new data is different from the old data, write and reload the page
       localStorage.setItem("links", JSON.stringify(dataNew));
       window.location.reload();
     });
 }
 
 function handleSubmit(e) {
-  //read the form data
+  e.preventDefault();
   const favUrl = document.getElementById("link-name").value;
   localStorage.setItem("favUrl", favUrl);
   deleteLocalStorage();
 }
 
-const AddFav = (data) => {
-  let FavUrl = localStorage.getItem("favUrl");
+const AddFav = () => {
+  const [favUrl, setFavUrl] = useState("");
+
+  useEffect(() => {
+    const storedFavUrl = localStorage.getItem("favUrl");
+    if (storedFavUrl) {
+      setFavUrl(storedFavUrl);
+    }
+  }, []);
 
   return (
     <div
       style={{
         height: "4em",
         width: "fit-content",
-        // backgroundColor: "#111",
         borderRadius: "1em",
         alignContent: "center",
         justifyContent: "center",
@@ -39,36 +43,40 @@ const AddFav = (data) => {
         transform: "translateX(-50%)",
         position: "fixed",
       }}
-
       className="bottom-bar"
     >
-      <input
-        type="text"
-        id="link-name"
-        name="link-name"
-        required
-        style={{ width: "25em", height: "2em", borderRadius: "1em", border: "none", padding: "0 1em", margin: "0 0.5em" }}
-        border="none"
-        placeholder="Enter your favorite URL"
-        onChange={(e) => {
-          FavUrl = e.target.value;
-        }}
-        value={FavUrl}
-      ></input>
-      <button
-        type="submit"
-        onClick={handleSubmit}
-        style={{
-          width: "10em",
-          height: "2em",
-          borderRadius: "1em",
-          backgroundColor: "#333",
-          color: "#fff",
-          border: "none",
-        }}
-      >
-        Update Link
-      </button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="link-name"
+          name="link-name"
+          required
+          style={{
+            width: "25em",
+            height: "2em",
+            borderRadius: "1em",
+            border: "none",
+            padding: "0 1em",
+            margin: "0 0.5em",
+          }}
+          placeholder="Enter your favorite URL"
+          onChange={(e) => setFavUrl(e.target.value)}
+          value={favUrl}
+        />
+        <button
+          type="submit"
+          style={{
+            width: "10em",
+            height: "2em",
+            borderRadius: "1em",
+            backgroundColor: "#333",
+            color: "#fff",
+            border: "none",
+          }}
+        >
+          Update Link
+        </button>
+      </form>
     </div>
   );
 };
